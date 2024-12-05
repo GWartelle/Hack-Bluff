be56f6c0989c4db570c54f4ea0d7a3666420e53d
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-12-05 09:14:25 +0100

    Update daisyui import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -11,5 +13,5 @@ export default {
       extend: {},
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
